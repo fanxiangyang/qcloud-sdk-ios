afe819cc1480957329b891a0837e446518002c9b
fanxiangyang <fanxiangyang_heda@163.com>
2026-04-07 21:00:26 +0800

    Add conditional import for netinet6 header
    <netinet6/in6.h> delete

--- QCloudCore/Classes/Base/QCLOUDRestNet/reachability/QCloudReachability.m
+++ QCloudCore/Classes/Base/QCLOUDRestNet/reachability/QCloudReachability.m
@@ -29,9 +29,6 @@
 
 #import <sys/socket.h>
 #import <netinet/in.h>
-#if __clang_major__ < 21 && __has_include(<netinet6/in6.h>)
-#import <netinet6/in6.h>
-#endif
 #import <arpa/inet.h>
 #import <ifaddrs.h>
 #import <netdb.h>
